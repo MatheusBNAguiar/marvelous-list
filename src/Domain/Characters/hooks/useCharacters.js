0d98f35f0f03bdb1b041b186ca576a03bc363ba4
Matheus Aguiar <matheus.bnaguiar@gmail.com>
2020-10-04 20:56:56 -0300

    Fix pagination issues

--- src/Domain/Characters/hooks/useCharacters.js
+++ src/Domain/Characters/hooks/useCharacters.js
@@ -1,11 +1,17 @@
 import { CharactersService } from 'Core/Services/Characters'
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { usePaginationItemQuantity } from 'Shared/hooks/usePaginationItemQuantity'
 import { usePaginatedQuery } from 'react-query'
 
 export const useCharacters = (characterStartsWith = '') => {
   const [page, setPage] = useState(1)
   const paginationQuantity = usePaginationItemQuantity()
+
+  useEffect(() => {
+    if (characterStartsWith) {
+      setPage(1)
+    }
+  }, [characterStartsWith])
 
   const getCharacters = useCallback(() => paginationQuantity && CharactersService.listCharacters({ page, paginationQuantity, characterStartsWith }), [page, paginationQuantity, characterStartsWith])
   const { isFetching, error, resolvedData: { results: characters = [], meta = {} } = {} } = usePaginatedQuery(['characters', { page, paginationQuantity, characterStartsWith }], getCharacters)
