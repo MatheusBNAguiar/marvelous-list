9bcb0bf376446925e3c7a3fc0269af87a1464812
Matheus Aguiar <matheus.bnaguiar@gmail.com>
2020-10-04 12:11:22 -0300

    Reduce data

--- src/Shared/hooks/usePaginationItemQuantity.js
+++ src/Shared/hooks/usePaginationItemQuantity.js
@@ -11,7 +11,7 @@ export const usePaginationItemQuantity = () => {
       setPaginationState(true)
       // Only running one time, to avoid any recalculations if the user only resizes its screen or rotate his cellphone
       if (width >= 1024) {
-        setPaginationQuantity(40)
+        setPaginationQuantity(30)
       } else if (width > 420) {
         setPaginationQuantity(20)
       } else {
